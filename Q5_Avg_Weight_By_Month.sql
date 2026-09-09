ecaa4571c359f3d94ccdb73494e40c57e65c0864
Miranda9620 <mirandarodriguez711@yahoo.com>
2026-09-08 19:05:53 -0500

    Modify ORDER BY clause for month-specific sorting
    Updated the ORDER BY clause to sort by specific months.

--- Q5_Avg_Weight_By_Month.sql
+++ Q5_Avg_Weight_By_Month.sql
@@ -3,4 +3,9 @@ SELECT
  ROUND(AVG(Body_Weight), 1) AS Avg_Body_Weight
 FROM `my-project-262026.barreras_fitness.client_data`
 GROUP BY Time_Period
-ORDER BY Avg_Body_Weight DESC;
+ORDER BY 
+   CASE
+   WHEN Time_Period = 'May' THEN 1
+   WHEN Time-Period = 'June' THEN 2
+   WHEN Time_Period = 'July' THEN 3
+END;
